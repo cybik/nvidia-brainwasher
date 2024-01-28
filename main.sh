d3a57b3afcd104362e7a7991ec5243197a0053dc
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-01-28 21:35:58 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -13,7 +13,7 @@ echo 'Pin: release c=ubuntu' >> /etc/apt/preferences.d/0-a
 echo 'Pin-Priority: 1000' >> /etc/apt/preferences.d/0-a
 # Clone Upstream
 apt update -y
-apt show nvidia-driver-$DRIVER 2>&1 | grep -v "does not have a stable" | grep Version: | head -n1 | cut -f2 -d":" | tr -d ' ' > new_nvidia.txt
+apt show nvidia-driver-$DRIVER 2>&1 | grep -v "does not have a stable" | grep Version: | head -n1 | cut -f2 -d":" | cut -f1,2,3 -d"." | cut -f1 -d"-" | tr -d ' ' > new_nvidia.txt
 apt download nvidia-driver-"$DRIVER"
 #wget https://ppa.launchpadcontent.net/graphics-drivers/ppa/ubuntu/pool/main/n/nvidia-graphics-drivers-545/nvidia-driver-545_545.113.01-0ubuntu3_amd64.deb
 ar -x ./nvidia-driver-"$DRIVER"*.deb
@@ -23,20 +23,17 @@ tar -xf ./data.tar.* -C ./nvidia-driver-"$DRIVER"/
 sed -i "s#nvidia-dkms-"$DRIVER"#nvidia-pika-kernel-module-"$DRIVER" | nvidia-dkms-"$DRIVER"#" ./nvidia-driver-"$DRIVER"/DEBIAN/control
 sed -i "s#$(cat ./nvidia-driver-"$DRIVER"/DEBIAN/control | grep "Version: ")#$(cat ./nvidia-driver-"$DRIVER"/DEBIAN/control | grep "Version: ")-100pika2#" ./nvidia-driver-"$DRIVER"/DEBIAN/control
 
-pika_nvidia=$(cat ./pika_nvidia.txt)
-new_nvidia=$(cat ./new_nvidia.txt)
-
-if ((new_nvidia >= pika_nvidia)); then
-  echo "driver up to date"
-  exit 0
-fi
-
 if echo "$(cat ./nvidia-driver-"$DRIVER"/DEBIAN/control | grep "Version: ")-100pika2" | grep -v "$(cat ./pika_nvidia.txt)"
 then
-  echo "driver up to date"
+  echo "driver already built"
   exit 0
 fi
 
+
+if cat ./pika_nvidia.txt | grep "$(cat ./new_nvidia.txt)"
+  echo "driver up to date"
+  exit 0
+fi
 
 apt install -y devscripts
 
