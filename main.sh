90475fcafd5c0e6c3ebdd21cb9343043bf9be463
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-01-28 21:25:32 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,18 +2,18 @@
 DRIVER=545
 DEBIAN_FRONTEND=noninteractive
 
+
+apt show nvidia-driver-$DRIVER 2>&1 | grep -v "does not have a stable" | grep Version: | head -n1 | cut -f2 -d":" | tr -d ' ' > pika_nvidia.txt
+
 rm -rfv /etc/apt/preferences.d/*
-echo 'Package: *' > /etc/apt/preferences.d/0-a
-echo 'Pin: release c=main' >> /etc/apt/preferences.d/0-a
-echo 'Pin-Priority: 390' >> /etc/apt/preferences.d/0-a
-echo 'Package: *' >> /etc/apt/preferences.d/0-a
-echo 'Pin: release c=external' >> /etc/apt/preferences.d/0-a
-echo 'Pin-Priority: 390' >> /etc/apt/preferences.d/0-a
+echo 'Pin: release c=external' > /etc/apt/preferences.d/0-a
+echo 'Pin-Priority: 1000' >> /etc/apt/preferences.d/0-a
 echo 'Package: *' >> /etc/apt/preferences.d/0-a
 echo 'Pin: release c=ubuntu' >> /etc/apt/preferences.d/0-a
-echo 'Pin-Priority: 390' >> /etc/apt/preferences.d/0-a
+echo 'Pin-Priority: 1000' >> /etc/apt/preferences.d/0-a
 # Clone Upstream
 apt update -y
+apt show nvidia-driver-$DRIVER 2>&1 | grep -v "does not have a stable" | grep Version: | head -n1 | cut -f2 -d":" | tr -d ' ' > new_nvidia.txt
 apt download nvidia-driver-"$DRIVER"
 #wget https://ppa.launchpadcontent.net/graphics-drivers/ppa/ubuntu/pool/main/n/nvidia-graphics-drivers-545/nvidia-driver-545_545.113.01-0ubuntu3_amd64.deb
 ar -x ./nvidia-driver-"$DRIVER"*.deb
@@ -23,11 +23,18 @@ tar -xf ./data.tar.* -C ./nvidia-driver-"$DRIVER"/
 sed -i "s#nvidia-dkms-"$DRIVER"#nvidia-pika-kernel-module-"$DRIVER" | nvidia-dkms-"$DRIVER"#" ./nvidia-driver-"$DRIVER"/DEBIAN/control
 sed -i "s#$(cat ./nvidia-driver-"$DRIVER"/DEBIAN/control | grep "Version: ")#$(cat ./nvidia-driver-"$DRIVER"/DEBIAN/control | grep "Version: ")-100pika2#" ./nvidia-driver-"$DRIVER"/DEBIAN/control
 
-if echo "$(cat ./nvidia-driver-"$DRIVER"/DEBIAN/control | grep "Version: ")-100pika2" | grep -v "$(apt show nvidia-driver-$DRIVER 2>&1 | grep -v "does not have a stable" | grep Version: | head -n1 | cut -f2 -d":" | tr -d ' ')"
+if [[ "$(cat ./pika_nvidia.txt)" >= "$(cat ./new_nvidia.txt)" ]]
 then
   echo "driver up to date"
   exit 0
 fi
+
+if echo "$(cat ./nvidia-driver-"$DRIVER"/DEBIAN/control | grep "Version: ")-100pika2" | grep -v "$(cat ./pika_nvidia.txt)"
+then
+  echo "driver up to date"
+  exit 0
+fi
+
 
 apt install -y devscripts
 
