38b1d8a7b32aa83bb2700a48f760916f8d0d896c
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-01-28 20:12:33 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,9 +3,6 @@ DRIVER=545
 DEBIAN_FRONTEND=noninteractive
 
 # Clone Upstream
-echo 'Package: *' > /etc/apt/preferences.d/0-a
-echo 'Pin: release c=main' >> /etc/apt/preferences.d/0-a
-echo 'Pin-Priority: 450' >> /etc/apt/preferences.d/0-a
 apt update -y
 apt download nvidia-driver-"$DRIVER"
 #wget https://ppa.launchpadcontent.net/graphics-drivers/ppa/ubuntu/pool/main/n/nvidia-graphics-drivers-545/nvidia-driver-545_545.113.01-0ubuntu3_amd64.deb
@@ -15,6 +12,12 @@ tar -xf ./control.tar.* -C ./nvidia-driver-"$DRIVER"/DEBIAN/
 tar -xf ./data.tar.* -C ./nvidia-driver-"$DRIVER"/
 sed -i "s#nvidia-dkms-"$DRIVER"#nvidia-pika-kernel-module-"$DRIVER" | nvidia-dkms-"$DRIVER"#" ./nvidia-driver-"$DRIVER"/DEBIAN/control
 sed -i "s#$(cat ./nvidia-driver-"$DRIVER"/DEBIAN/control | grep "Version: ")#$(cat ./nvidia-driver-"$DRIVER"/DEBIAN/control | grep "Version: ")-100pika2#" ./nvidia-driver-"$DRIVER"/DEBIAN/control
+
+if echo "$(apt show kernel-pika  2>&1 | grep -v "does not have a stable" | grep Version: | head -n1 | cut -f2 -d":" | cut -f1,2,3 -d"." | cut -f1 -d"-" | tr -d ' ')" | grep "$(cat ./nvidia-driver-"$DRIVER"/DEBIAN/control | grep "Version: ")-100pika2"
+then
+  echo "driver up to date"
+  exit 0
+fi
 
 apt install -y devscripts
 
